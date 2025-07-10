831a7fcb650b929548f28af1652d094e9fceeaf4
Antarcticpx <antarcticpx@gmail.com>
2025-07-10 12:02:00 +0700

    fix : fixed error in create feature for koor teknis

--- app/Http/Controllers/Api/KoorTeknisController.php
+++ app/Http/Controllers/Api/KoorTeknisController.php
@@ -3,9 +3,13 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Document;
 use App\Models\koor_teknis;
+use App\Models\marketing;
 use Carbon\Carbon;
+use Illuminate\Contracts\Support\ValidatedData;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rule;
@@ -22,7 +26,7 @@ class KoorTeknisController extends Controller
         ], 200);
     }
 
-    public function store(Request $request, $id) {
+    public function store(Request $request) {
         $validator = Validator::make($request->all(), [
             'tgl_masuk' => 'required|max:255',
             'status' => 'required|in:accept koor teknis, decline koor teknis',
@@ -36,6 +40,23 @@ class KoorTeknisController extends Controller
                 'data_koor_teknis' => $validator->errors()
             ], 401); 
         }
+
+        // $marketing = marketing::with('document')->where('document_id', );
+
+        $user = Auth::user();
+        $id = $user->id;
+
+        $document = Document::where('user_id', $id)->first();
+        if (!$document) {
+            return response()->json(['message' => 'document not found for this user'], 404);
+        }
+        $marketing = marketing::where('document_id', $document->id)->first();
+        if (!$marketing) {
+            return response()->json(['message' => 'document not found for this user'], 404);
+        }
+
+        $validatedData["marketing_id"] = $marketing->id;
+      
 
         $file = $request->file('document_path');
         $validatedData = $validator->validated();
@@ -51,7 +72,7 @@ class KoorTeknisController extends Controller
 
         $createdData = koor_teknis::create([
             'user_id' => $request->user()->id,
-            'marketing_id' => $id,
+            'marketing_id' => $marketing->id,
             'tgl_masuk' => $validatedData["tgl_masuk"],
             'status' => $validatedData["status"],
             'document_path' => $url
